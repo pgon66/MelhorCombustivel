8c5b26e6e42f8a2e69b7b390c67d69d9112a2cb5
pgon66 <pedrotengon@gmail.com>
2022-08-21 16:17:24 -0300

    first commit

---
+++ app/src/main/java/pr/senac/br/melhorcombustivel/MainActivity.java
@@ -0,0 +1,59 @@
+package pr.senac.br.melhorcombustivel;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.annotation.Nullable;
+
+
+public class MainActivity extends Activity {
+
+    public static final String EXTRA_VALOR_GASOLINA = "VALOR_GASOLINA";
+    public static final String EXTRA_VALOR_ETANOL = "VALOR_ETANOL";
+    public static final String EXTRA_CONSUMO_GASOLINA = "CONSUMO_GASOLINA";
+    public static final String EXTRA_CONSUMO_ETANOL = "CONSUMO_ETANOL";
+    private Button buttonCalcular;
+    private EditText valorGasolina;
+    private EditText valorEtanol;
+    private EditText consumoGasolina;
+    private EditText consumoEtanol;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceSate) {
+        super.onCreate(savedInstanceSate);
+        setContentView(R.layout.activity_main);
+        valorGasolina = findViewById(R.id.valorGasolina);
+        valorEtanol = findViewById(R.id.valorEtanol);
+        consumoGasolina = findViewById(R.id.consumoGasolina);
+        consumoEtanol = findViewById(R.id.consumoEtanol);
+        buttonCalcular = findViewById(R.id.back_button);
+        buttonCalcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                changeToResultScreen();
+            }
+        });
+    }
+
+    private void changeToResultScreen() {
+        Intent intent = new Intent( this, ResultActivity.class);
+        EditText valorGasolina = (EditText) findViewById(R.id.valorGasolina);
+        EditText valorEtanol = (EditText) findViewById(R.id.valorEtanol);
+        intent.putExtra(EXTRA_VALOR_GASOLINA, valorGasolina.getText().toString());
+        intent.putExtra(EXTRA_VALOR_ETANOL, valorEtanol.getText().toString());
+
+        EditText consumoGasolina = (EditText) findViewById(R.id.consumoGasolina);
+        EditText consumoEtanol = (EditText) findViewById(R.id.consumoEtanol);
+        intent.putExtra(EXTRA_CONSUMO_GASOLINA, consumoGasolina.getText().toString());
+        intent.putExtra(EXTRA_CONSUMO_ETANOL, consumoEtanol.getText().toString());
+
+        startActivity(intent);
+    }
+
+}
---
+++ app/src/main/java/pr/senac/br/melhorcombustivel/ResultActivity.java
@@ -0,0 +1,77 @@
+package pr.senac.br.melhorcombustivel;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+
+
+public class ResultActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+        Intent intent = getIntent();
+
+        String valorGasolina = intent.getStringExtra(pr.senac.br.melhorcombustivel.MainActivity.EXTRA_VALOR_GASOLINA);
+        String valorEtanol = intent.getStringExtra(pr.senac.br.melhorcombustivel.MainActivity.EXTRA_VALOR_ETANOL);
+        String consumoGasolina = intent.getStringExtra(pr.senac.br.melhorcombustivel.MainActivity.EXTRA_CONSUMO_GASOLINA);
+        String consumoEtanol = intent.getStringExtra(pr.senac.br.melhorcombustivel.MainActivity.EXTRA_CONSUMO_ETANOL);
+
+        TextView resultadoView = findViewById(R.id.ResultTextView);
+        TextView resultadoValor = findViewById(R.id.resultadoValor);
+        TextView resultadoKmL = findViewById(R.id.resultadoKmL);
+        TextView resultadoValorEtanol = findViewById(R.id.resultadoValorEtanol);
+        TextView resultadoKmLEtanol = findViewById(R.id.resultadoKmLEtanol);
+        TextView resultadoEconomia = findViewById(R.id.resultadoEconomia);
+        TextView ResultadoConsumo = findViewById(R.id.resultadoConsumo);
+
+        double valorGasolinaBack = Double.parseDouble(valorGasolina);
+        double valorEtanolBack = Double.parseDouble(valorEtanol);
+        double consumoGasolinaBack = Double.parseDouble(consumoGasolina);
+        double consumoEtanolBack = Double.parseDouble(consumoEtanol);
+        double resultadoConsumoGasolina = valorGasolinaBack / consumoGasolinaBack;
+        double resultadoConsumoEtanol = valorEtanolBack / consumoEtanolBack;
+        double resultadoConsumo = 0;
+        double resultado = resultadoConsumoEtanol / resultadoConsumoGasolina;
+        double relacaoCombustivel = (valorEtanolBack / valorGasolinaBack) * 100;
+
+        resultadoValor.setText("R$ " + valorGasolina);
+        resultadoKmL.setText(consumoGasolina);
+        resultadoValorEtanol.setText("R$ " + valorEtanol);
+        resultadoKmLEtanol.setText(consumoEtanol);
+
+        ResultadoConsumo.setText(
+                String.format("Relação Etanol/Gasolina %.2f", relacaoCombustivel) + ("%") +
+                String.format("\nGasto com Gasolina R$%.2f", resultadoConsumoGasolina) +
+                String.format("\nGasto com Etanol R$%.2f", resultadoConsumoEtanol));
+
+        if (resultadoConsumoGasolina < resultadoConsumoEtanol) {
+            resultadoConsumo = resultadoConsumoEtanol - resultadoConsumoGasolina;
+            resultadoEconomia.setText(String.format("Economia de R$%.2f", resultadoConsumo) + " por litro");
+        } else {
+            resultadoConsumo = resultadoConsumoGasolina - resultadoConsumoEtanol;
+            resultadoEconomia.setText(String.format("Economia de R$%.2f", resultadoConsumo) + " por litro");
+        }
+
+
+        if(resultado >= 0.7) {
+            resultadoView.setText("Abasteça com Gasolina");
+        } else {
+            resultadoView.setText("Abasteça com Etanol");
+        }
+
+        Button backButton = findViewById(R.id.back_button);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ResultActivity.super.onBackPressed();
+            }
+        });
+    }
+}
